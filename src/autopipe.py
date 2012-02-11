057a0e2167748b10053ae5d34e9dfdcff2b7119c
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-11 07:38:01 -0300

    improved multiplatform pipe

--- src/autopipe.py
+++ src/autopipe.py
@@ -4,8 +4,28 @@
 import os
 import sys
 
-if os.name in ("nt", "posix"):
+
+TKPIPE = os.name in ("nt")
+
+if TKPIPE:
     import tkpipe
-    pipe = tkpipe.Tkpipe()
+    TKPIPE = tkpipe.Tkpipe()
     sys.stdout = pipe.default("green")
     sys.stderr = pipe.default("red")
+
+
+def showimage(image):
+    if TKPIPE:
+        TKPIPE.writeimage(image)
+    else:
+        image.show()
+
+def color(message, color="blue"):
+    if TKPIPE:
+        TKPIPE.write(message, color)
+    else:
+        sys.stderr.write(message)
+
+blue = lambda message:color(message, "blue")
+red = lambda message:color(message, "red")
+green = lambda message:color(message, "green")
