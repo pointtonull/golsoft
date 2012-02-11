6aa3eb3941ae8fe9a82e76ad3feb55ef04f09e5c
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-11 04:46:09 -0300

    added a pure python nice downloader

---
+++ src/downloader.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+import urllib
+import os
+
+class URLOpener(urllib.FancyURLopener):
+    def http_error_206(self, url, fp, errcode, errmsg, headers, data=None):
+        pass
+
+def downloader(url, dest, retries=5):
+    if retries == 0:
+        return False
+    opener = URLOpener()
+    connection = opener.open(url)
+    totalsize = int(connection.headers['Content-Length'])
+
+    if os.path.exists(dest):
+        output = open(dest, "ab")
+        currentsize = os.path.getsize(dest)
+        opener.addheader("Range","bytes=%s-" % currentsize)
+        connection = opener.open(url)
+    else:
+        output = open(dest, "wb")
+        currentsize = 0
+
+    print currentsize, totalsize
+    while currentsize < totalsize:
+        data = connection.read(8192)
+        output.write(data)
+        currentsize += len(data)
+        print "%d, " % (100 * currentsize / totalsize),
+    
+    if totalsize == currentsize:
+        print ""
+        return True
+    else:
+        return downloader(url, dest, retries - 1)
