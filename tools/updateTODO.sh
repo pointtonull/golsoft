92ab2e57f9c337fbc3a1d84b189ae160a258d864
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-17 14:20:41 -0300

    added DONE and FULL page 	modified:   tools/updateTODO.sh
    typo
    
    updated docs
    
    updated docs
    
    updated docs
    
    updated docs
    
    updated docs

--- tools/updateTODO.sh
+++ tools/updateTODO.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 NOTEBOOK="~/documentos/carlos/apuntes/zim"
-PAGE="Home:Seminario:TODO"
-zim --export --template Presentation --output=docs "$NOTEBOOK" "$PAGE"
-awk '
-    {
-        gsub("file:///usr/share/zim/", "")
-    }
-    /./
-' docs/TODO.html > docs/TODO.tmp
+TODO="Home:Seminario:TODO"
+DONE="Home:Seminario:TODO:DONE"
+FULL="Home:Seminario:TODO:FULL"
 
+zim --export --template Presentation --output=docs "$NOTEBOOK" "$TODO"
+zim --export --template Presentation --output=docs "$NOTEBOOK" "$DONE"
+zim --export --template Presentation --output=docs "$NOTEBOOK" "$FULL"
+
+awk '{gsub("file:///usr/share/zim/", "");
+gsub("./TODO/", "./TODO.")}/./' docs/TODO.html > docs/TODO.tmp
 mv docs/TODO.tmp docs/TODO.html
-git add docs/TODO.html
-git commit -m "updated TODO"
+
+awk '{gsub("file:///usr/share/zim/", "")}/./' docs/DONE.html > docs/DONE.tmp
+mv docs/DONE.tmp docs/TODO.DONE.html
+
+awk '{gsub("file:///usr/share/zim/", "")}/./' docs/FULL.html > docs/FULL.tmp
+mv docs/FULL.tmp docs/TODO.FULL.html
+
+ln -f ~/documentos/carlos/seminario/documento/Maestro.pdf docs/seminario\ Carlos.pdf
+
+git add docs/*.html docs/*.pdf
+git commit -m "updated docs"
 git push
