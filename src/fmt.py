97059eb627ff5f17be2a586127550dee3f699d42
Carlos Cabrera <point_to_null@esdebian.org>
2012-02-20 15:11:25 -0300

    new fmt interface

--- src/fmt.py
+++ src/fmt.py
@@ -9,24 +9,23 @@ lp_ft_magnitude = logpolar(ft_magnitude)
 fmt = fft(lp_ft_magnitude)
 """
 
-from numpy import pi, sin, cos, exp, ndarray, asarray
+from numpy import pi, sin, cos, exp, ndarray, asarray, log
 from scipy.ndimage import geometric_transform
 
 tau = 2 * pi
 
-def logpolar(array, scale=1):
+def get_logpolar(array):
     rows, cols = array.shape
-    out_rows = rows * scale
-    out_cols = cols * scale
+    out_rows = rows
+    out_cols = cols
     row0 = rows / 2.
     col0 = cols / 2.
-    rho_scalar = 2 * exp(1) / out_rows
     theta_scalar = tau / out_cols
 
     def out2in(dst_coords):
-        rho, theta = dst_coords
-        rho = rho * rho_scalar
-        theta *= theta_scalar
+        theta, radius = dst_coords
+        rho = log(radius) # radius = e^rho 
+        theta = theta * theta_scalar - pi
         row_from = exp(rho) * cos(theta) + row0
         col_from = exp(rho) * sin(theta) + col0
         return row_from, col_from
