d3dcce0053f8b348602cdd24308c24509e051b93
Carlos Cabrera <point_to_null@esdebian.org>
2012-04-02 06:51:15 -0300

    intial commit

--- src/pea.py
+++ src/pea.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 
+"""
+Simple implementation of the Angular Spectrum Method to reconstruct lensless
+holograms
+"""
+
 from automask import get_circles, get_holed_window, get_mask
-from autopipe import showimage, blue
+from fmt import get_shiftedfft, get_ifft
+from image import equalize, get_intensity, get_centered
+from numpy import exp, cos, sqrt
+from scipy import optimize
 import cache
-from image import equalize, logscale, get_intensity, get_centered, imread
-from fmt import get_shiftedfft, get_shiftedifft, get_fft, get_ifft
-from numpy import exp, cos, sin, sqrt, angle
-from scipy import ndimage, optimize
 import numpy as np
-import sys
 
 
 tau = 6.283185307179586
@@ -23,8 +26,10 @@ MASK_R_SCALE = 3
 
 @cache.hybrid
 def apply_mask(array):
-    oarray = array
-    array = get_centered(array) # TODO: validate (ACM)
+    """
+    Try to filter out spurious data.
+    """
+    array = get_centered(array)
     shape = array.shape
     intensity = equalize(array)
 
@@ -37,13 +42,16 @@ def apply_mask(array):
     window = get_holed_window(windowmaker, real_order[2] * MASK_R_SCALE, 10)
     mask = get_mask(shape, window)
 
-    masked = get_centered(mask * array)
-
+    masked = get_centered(mask * centered)
     return masked
 
 
 @cache.hybrid
 def get_ref_beam(shape, alpha=90, beta=90):
+    """
+    Generate a reference beam array given the shape of the hologram and the
+    directors angles
+    """
     maxrow = shape[0] / 2
     maxcol = shape[1] / 2
     minrow, mincol = -maxrow, -maxcol
@@ -70,7 +78,7 @@ def get_pea(hologram, distance, alpha=90, beta=90):
     rhologram = ref_beam * hologram
 
     frh = get_shiftedfft(rhologram)
-    cfrh = get_centered(frh)
+    frh = get_centered(frh)
     masked = apply_mask(frh)
     maxrow = shape[0] / 2
     maxcol = shape[1] / 2
@@ -82,47 +90,44 @@ def get_pea(hologram, distance, alpha=90, beta=90):
     propagation_array = get_centered(propagation_array)
     propagated = propagation_array * masked
     reconstructed = get_ifft(propagated)
-    wrapped_phase = angle(reconstructed)
+    wrapped_phase = np.angle(reconstructed)
     return wrapped_phase
-    unwrapped = unwrap(wrapped)
 
 
-def frange(mean, radius, amount):
-    if amount == 1:
-        start = mean
-        step = 0.
-    else:
-        start = mean - radius
-        step = (radius * 2) / (amount - 1.)
-    for stepn in xrange(amount):
-        yield start + stepn * step
-
-
-def get_strips_angle_radius(array):
-    shape = array.shape
+def get_strips_angle_radius(hologram):
+    """
+    Calculate the inclination angle and raiun for the given hologram strips
+    """
+    shape = hologram.shape
     diagonal = sum((dim ** 2 for dim in shape)) ** .5
     center = [dim / 2. for dim in shape]
 
-    fft = get_intensity(get_shiftedfft(array))
+    fft = get_intensity(get_shiftedfft(hologram))
     peak = get_circles(fft, 2)[1][1]
     peak = peak[0] - center[0], peak[1] - center[1]
     radius = (peak[0] ** 2 + peak[1] ** 2) ** .5 / diagonal * 2.
     angle = (1.570796325 - np.arctan2(*peak)) % 3.1415926536
-    return angle, radius 
+    return angle, radius
 
 
 @cache.hybrid
-def guess_angles(array):
-    angle, radius = get_strips_angle_radius(array)
-    ref_shape = [dim / 2 for dim in array.shape]
+def guess_angles(hologram):
+    """
+    Uses ML algoritms to guess the corrects directors angles for the given
+    hologram
+    """
+    angle, radius = get_strips_angle_radius(hologram)
+    ref_shape = [dim / 2 for dim in hologram.shape]
 
     def get_angles_fitness(args):
+        """
+        Learning fitness function
+        """
         ref_beam = get_ref_beam(ref_shape, args[0], args[1])
         new_angle, new_radius = get_strips_angle_radius(ref_beam)
         angle_diff = (new_angle - angle) / 3.14159265
         radius_diff = new_radius - radius
         distance = (angle_diff ** 2 + radius_diff ** 2) ** .5
-        print distance, args
         return distance
 
     xinit = np.array([tau/4., tau/4.])
@@ -132,14 +137,3 @@ def guess_angles(array):
 
     xend = optimizer(get_angles_fitness, xinit)
     return xend[0], xend[1]
-
-
-
-
-if __name__ == "__main__":
-    exit(main())
-
-
-def main():
-    pass
-
