fb353f4e662eb290b2c544f2df7c6c57a115fc0d
Carlos Cabrera <point_to_null@esdebian.org>
2012-07-17 03:14:53 -0300

    no holograms

---
+++ src/todropbox.sh
@@ -0,0 +1,7 @@
+rsync -ar --no-owner --no-g --modify-window=5 --delete ./ ~/documentos/carlos/Dropbox/golsoft/src
+cd ~/documentos/carlos/Dropbox/golsoft/src
+todos *.py
+todos *.pyw
+todos *.h
+todos *.c
+todos *.cp
