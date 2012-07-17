ac13f234573d701fac84edb04e4cd6e1822c028c
Carlos Cabrera <point_to_null@esdebian.org>
2012-07-17 03:19:19 -0300

    unified git and dropbox push

---
+++ src/push.sh
@@ -0,0 +1,12 @@
+git commit --interactive
+while git status|grep "modified:" > /dev/null; do
+    git commit --interactive
+done && git push
+git checkout master
+git merge experimental
+git push
+git checkout experimental
+rsync -ar --no-owner --no-g --modify-window=5 --delete ./ ~/documentos/carlos/Dropbox/golsoft/src
+cd ~/documentos/carlos/Dropbox/golsoft/src
+todos *.py
+todos *.pyw
--- src/todropbox.sh
+++
@@ -1,7 +0,0 @@
-rsync -ar --no-owner --no-g --modify-window=5 --delete ./ ~/documentos/carlos/Dropbox/golsoft/src
-cd ~/documentos/carlos/Dropbox/golsoft/src
-todos *.py
-todos *.pyw
-todos *.h
-todos *.c
-todos *.cp
