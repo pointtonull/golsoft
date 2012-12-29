f87503338c156974c18ecf3cf81381e3ee9afc0c
Carlos Cabrera <point_to_null@esdebian.org>
2012-12-29 19:37:35 -0300

    paraboloid fitter to compensate lens deformation

--- src/minimize.py
+++ src/minimize.py
@@ -3,15 +3,18 @@
 
 from scipy import optimize
 
+import numpy as np
+
 def generic_minimizer(fitness_func, initial_guess, optimizers=None):
     """
     A common interface to various minimization algorithms
     """
-
+    
     if optimizers == None:
         optimizers = [
             optimize.fmin, # 66
             optimize.fmin_powell,
+#            optimize.leastsq,
         ]
 
     best_result = None
@@ -24,3 +27,38 @@ def generic_minimizer(fitness_func, initial_guess, optimizers=None):
 
     return best_guess
 
+
+def squared_error(array1, array2):
+    return ((array1 - array2) ** 2).sum()
+
+
+def get_paraboloid(x, y, a0, b0, a1, b1, c):
+    return (a0 * x + b0) ** 2 + (a1 * y + b1) ** 2 + c
+
+
+def get_fitted_paraboloid(data):
+    xs, ys = data.shape
+    x, y = np.mgrid[:xs, :ys]
+
+    def fitness((a0, b0, a1, b1, c)):
+        error = squared_error(data, get_paraboloid(x, y, a0, b0, a1, b1, c))
+        return error
+
+    params = generic_minimizer(fitness, [1] * 5)
+    return get_paraboloid(x, y, *params)
+
+
+def main():
+    from scipy.misc import lena
+    from autopipe import showimage
+    x, y = np.mgrid[:100, :100]
+    eye = lena()[200:300, 200:300]
+    data = get_paraboloid(x, y, 1, 2, 3, 4, 5)
+    noisy = data + eye
+    fitted = get_fitted_paraboloid(noisy)
+    showimage(eye, noisy, fitted, noisy - fitted)
+    return 0
+
+
+if __name__ == "__main__":
+    exit(main())
